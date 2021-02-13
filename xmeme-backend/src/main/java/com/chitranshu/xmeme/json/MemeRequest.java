4bb91704205ef312a9a275129edffb3a1c9391eb
Chitranshu Gour <chitranshugour67@gmail.com>
2021-02-13 18:59:51 +0530

    backend added

---
+++ xmeme-backend/src/main/java/com/chitranshu/xmeme/json/MemeRequest.java
@@ -0,0 +1,40 @@
+package com.chitranshu.xmeme.json;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MemeRequest {
+
+	/*
+	 * To accept the request from RequestBody.
+	 */
+
+	private String name;
+	private String caption;
+	private String url;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String imageUrl) {
+		this.url = imageUrl;
+	}
+
+}
