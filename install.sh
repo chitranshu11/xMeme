85106caf6793e6b7fd4f477803fe472c743ce20d
Chitranshu Gour <chitranshugour67@gmail.com>
2021-02-19 12:53:21 +0530

    j

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
+sudo apt-get update -y && sudo apt-get dist-upgrade -y
+sudo apt-get install -y openjdk-8-jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
 
+sudo apt update
+
+sudo apt-get install -y maven
+
+echo $JAVA_HOME
+export PATH=/opt/apache-maven-3.6.3/bin:$PATH
+
+
+# Installing MySQL
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" mysql-server;
 
 # Setting up the database
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('root');FLUSH PRIVILEGES;"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-#sudo mysql -u root -proot -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';FLUSH PRIVILEGES;"
-sudo mysql -e "Alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Current-Root-Password';FLush privileges;"
-sudo mysql -uroot -proot -e "Alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Current-Root-Password';FLush privileges;"
+sudo mysql -u root -proot -e "CREATE USER 'huhu'@'localhost' IDENTIFIED BY 'huhu';GRANT ALL PRIVILEGES ON *.* TO 'huhu'@'localhost';FLUSH PRIVILEGES;"
 sudo mysql -uroot -proot -e 'create database IF NOT EXISTS xmeme;'
 service mysql restart
-
-
-
 
 
 
