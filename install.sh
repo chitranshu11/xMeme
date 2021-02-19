25551a78f2a1e4400919a97f6610b8c5b5c88ee8
Chitranshu Gour <chitranshugour67@gmail.com>
2021-02-19 11:27:04 +0530

    k

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" mysql-server;
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('root');FLUSH PRIVILEGES;"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-sudo mysql -u root -proot -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';FLUSH PRIVILEGES;"
+#sudo mysql -u root -proot -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';FLUSH PRIVILEGES;"
 sudo mysql -uroot -proot -e 'create database IF NOT EXISTS xmeme;'
 service mysql restart
 
